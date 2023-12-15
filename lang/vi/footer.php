42598613979bfd50c5ea8dbd31ac1cf14c4465a4
code-tieumomo <code.tieumomo@gmail.com>
2023-12-16 01:16:35 +0700

    refactor: clear files

---
+++ lang/vi/footer.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Footer (Translate to Vietnamese)
+    |--------------------------------------------------------------------------
+    */
+
+    'join' => 'Tham gia',
+    'join_us_today' => 'Tham gia ngay hôm nay',
+    'subscribe_content' => '#Chúng tôi sẽ gửi những ưu đãi tốt nhất đến email của bạn.',
+    'enter_email_here' => 'Nhập email của bạn vào đây',
+
+];
